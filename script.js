a7ad10fbbd7f96cb780afddb53e9191690761093
elmar <elmarcera@gmail.com>
2023-11-21 14:08:05 +0800

    Coding Challenge #1

--- script.js
+++ script.js
@@ -833,87 +833,208 @@
 // Logical Assignment Operators
 /////////////////////////////////////////////////////////////////
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+//   orderDelivery: function ({
+//     starterIndex = 1,
+//     mainIndex = 0,
+//     time = '20:00',
+//     address,
+//   }) {
+//     console.log(
+//       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+//   },
 
-  orderPizza: function (mainIngredients, ...otherIngrdients) {
-    console.log(mainIngredients);
-    console.log(otherIngrdients);
+//   orderPizza: function (mainIngredients, ...otherIngrdients) {
+//     console.log(mainIngredients);
+//     console.log(otherIngrdients);
+//   },
+// };
+
+// // are three new so-called logical assignment operators
+// // that were introduced in ES 2021.
+
+// const rest1 = {
+//   name: 'Capri',
+//   // numGuests: 20,
+//   numGuests: 0,
+// };
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
+
+// //OR Assignment Operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests ||= 10;
+// // rest2.numGuests ||= 10;
+
+// // Nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// //and Assignment Operator
+
+// // And so basically, what the logical
+// // and assignment operator does
+// // is to assign a value to a variable
+// // if it is currently truthy.
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
+
+//////////////////////////////////////////////////////////////////////////
+//  Data Structures, Modern Operators and Strings
+//  Coding Challenge #1
+//////////////////////////////////////////////////////////////////////////
+
+//  We're building a football betting app (soccer for my American friends �)!
+//  Suppose we get data from a web service about a certain game ('game' variable on
+//  next page). In this challenge we're gonna work with that data.
+//  Your tasks:
+
+//  1. Create one player array for each team (variables 'players1' and
+//  'players2')
+
+//  2. The first player in any player array is the goalkeeper and the others are field
+//  players. For Bayern Munich (team 1) create one variable ('gk') with the
+//  goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+//  field players
+
+//  3. Create an array 'allPlayers' containing all players of both teams (22
+//  players)
+
+//  4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+//  new array ('players1Final') containing all the original team1 players plus
+//  'Thiago', 'Coutinho' and 'Perisic'
+
+//  5. Based on the game.odds object, create one variable for each odd (called
+//  'team1', 'draw' and 'team2')
+
+//  6. Write a function ('printGoals') that receives an arbitrary number of player
+//  names (not an array) and prints each of them to the console, along with the
+//  number of goals that were scored in total (number of player names passed in)
+
+//  7. The team with the lower odd is more likely to win. Print to the console which
+//  team is more likely to win, without using an if/else statement or the ternary
+//  operator.
+
+//  Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+
+//  Then, call the function again with players from game.scored
+//  GOOD LUCK �
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-// are three new so-called logical assignment operators
-// that were introduced in ES 2021.
+//1 - destructuring  game.players
+const [players1, players2] = game.players;
+console.log(players1, players2);
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20,
-  numGuests: 0,
+//2 - destructuring - rest syntax
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3 - spread operator
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4 - adding element on array
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5
+// const { team1, x: draw, team2 } = game.odds;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6
+
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored `);
 };
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+//  Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored); // using the spread operator to pass an array of arguments
 
-//OR Assignment Operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-
-// Nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-
-//and Assignment Operator
-
-// And so basically, what the logical
-// and assignment operator does
-// is to assign a value to a variable
-// if it is currently truthy.
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1);
-console.log(rest2);
+// 7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
