59701f5e9355759c42998a1acb0dabf7de696cf1
elmar <elmarcera@gmail.com>
2023-11-22 13:23:07 +0800

    Coding Challenge #2

--- script.js
+++ script.js
@@ -1449,116 +1449,241 @@
 // Looping Objects: Object Keys, Values, and Entries
 //////////////////////////////////////////////////////////////////
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
 
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 12 + 12,
-  },
-};
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 12 + 12,
+//   },
+// };
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours,
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   openingHours,
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+//   // openingHours: {
+//   //   thu: {
+//   //     open: 12,
+//   //     close: 22,
+//   //   },
+//   //   fri: {
+//   //     open: 11,
+//   //     close: 23,
+//   //   },
+//   //   sat: {
+//   //     open: 0, // Open 24 hours
+//   //     close: 24,
+//   //   },
+//   // },
 
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order(starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+//   orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+//     console.log(
+//       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
 
-  orderPasta(ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
+//   orderPasta(ing1, ing2, ing3) {
+//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+//   },
 
-  orderPizza(mainIngredients, ...otherIngrdients) {
-    console.log(mainIngredients);
-    console.log(otherIngrdients);
-  },
-};
+//   orderPizza(mainIngredients, ...otherIngrdients) {
+//     console.log(mainIngredients);
+//     console.log(otherIngrdients);
+//   },
+// };
 
-// So we learned about the for of loop
-// to loop over a race, which remember is an Iterable,
-// but we can also loop over objects,
-// which are not Iterable,
-// but in an indirect way.
-// Now we have different options here,
-// depending on what exactly we want to loop over.
-// So do we want to loop over the objects, property names
-// over the values or both together.
-// And let's start by simply looping over property names.
-// And so remember they are also called keys.
-// Now, ultimately we will still have to use the for of loop
-// to loop over the array, but again,
-// we're going to do that in an indirect way.
-// So we're not actually looping over the object itself.
-// Instead, we're going to loop over, an array.
+// // So we learned about the for of loop
+// // to loop over a race, which remember is an Iterable,
+// // but we can also loop over objects,
+// // which are not Iterable,
+// // but in an indirect way.
+// // Now we have different options here,
+// // depending on what exactly we want to loop over.
+// // So do we want to loop over the objects, property names
+// // over the values or both together.
+// // And let's start by simply looping over property names.
+// // And so remember they are also called keys.
+// // Now, ultimately we will still have to use the for of loop
+// // to loop over the array, but again,
+// // we're going to do that in an indirect way.
+// // So we're not actually looping over the object itself.
+// // Instead, we're going to loop over, an array.
 
-//Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+// //Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-// console.log(`We are open on ${properties.length} days.`);
-// for (const day of Object.keys(openingHours)) {
-//   console.log(day);
+// // console.log(`We are open on ${properties.length} days.`);
+// // for (const day of Object.keys(openingHours)) {
+// //   console.log(day);
+// // }
+
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// // Property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// //Entire object
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
 // }
 
-let openStr = `We are open on ${properties.length} days: `;
+// // But if you had a more simple object with a simpler value,
+// // then you would, of course only do this here
+// // as destructuring, just a key and a value.
+// // And of course you could then give them other names
+// // as you would like.
 
-for (const day of properties) {
-  openStr += `${day}, `;
+// //[key, value]
+
+//////////////////////////////////////////////////////////////////////////
+// Coding Challenge #2
+//////////////////////////////////////////////////////////////////////////
+
+// Let's continue with our football betting app! Keep using the 'game' variable from
+// before.
+// Your tasks:
+
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names �
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
+// }
+
+// GOOD LUCK �
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+for (const [key, player] of game.scored.entries()) {
+  console.log(`Goal ${key + 1}: ${player}`);
 }
-console.log(openStr);
 
-// Property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
 
-//Entire object
-const entries = Object.entries(openingHours);
-console.log(entries);
+const odds = Object.values(game.odds);
 
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day} we open at ${open} and close at ${close}`);
+let average = 0;
+for (const odd of odds) {
+  average += odd;
 }
 
-// But if you had a more simple object with a simpler value,
-// then you would, of course only do this here
-// as destructuring, just a key and a value.
-// And of course you could then give them other names
-// as you would like.
+average /= odds.length;
+console.log(average);
 
-//[key, value]
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of victory ${teamStr}: ${odd} `);
+}
+
+// BONUS
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
+// }
+
+// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+console.log(scorers);
