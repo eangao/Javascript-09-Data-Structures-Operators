41b3a27b9a78e8f723dbffdfe01195dbac2c167c
elmar <elmarcera@gmail.com>
2023-11-23 14:42:07 +0800

    String Methods Practice

--- script.js
+++ script.js
@@ -2716,61 +2716,89 @@
 // Coding Challenge #4
 ////////////////////////////////////////////////////////////
 
-// Write a program that receives a list of variable names written in underscore_case
-// and convert them to camelCase.
-// The input will come from a textarea inserted into the DOM (see code below to
-// insert the elements), and conversion will happen when the button is pressed.
+// // Write a program that receives a list of variable names written in underscore_case
+// // and convert them to camelCase.
+// // The input will come from a textarea inserted into the DOM (see code below to
+// // insert the elements), and conversion will happen when the button is pressed.
 
-// Test data (pasted to textarea, including spaces):
-// underscore_case
-// first_name
-// Some_Variable
-//  calculate_AGE
-// delayed_departure
+// // Test data (pasted to textarea, including spaces):
+// // underscore_case
+// // first_name
+// // Some_Variable
+// //  calculate_AGE
+// // delayed_departure
 
-// Should produce this output (5 separate console.log outputs):
-// underscoreCase   ✅
-// firstName        ✅✅
-// someVariable     ✅✅✅
-// calculateAge     ✅✅✅✅
-// delayedDeparture ✅✅✅✅✅
+// // Should produce this output (5 separate console.log outputs):
+// // underscoreCase   ✅
+// // firstName        ✅✅
+// // someVariable     ✅✅✅
+// // calculateAge     ✅✅✅✅
+// // delayedDeparture ✅✅✅✅✅
 
-// Hints:
-// § Remember which character defines a new line in the textarea �
-// § The solution only needs to work for a variable made out of 2 words, like a_b
-// § Start without worrying about the ✅. Tackle that only after you have the variable
-// name conversion working �
-// § This challenge is difficult on purpose, so start watching the solution in case
-// you're stuck. Then pause and continue!
+// // Hints:
+// // § Remember which character defines a new line in the textarea �
+// // § The solution only needs to work for a variable made out of 2 words, like a_b
+// // § Start without worrying about the ✅. Tackle that only after you have the variable
+// // name conversion working �
+// // § This challenge is difficult on purpose, so start watching the solution in case
+// // you're stuck. Then pause and continue!
 
-// Afterwards, test with your own test data!
-// GOOD LUCK �
+// // Afterwards, test with your own test data!
+// // GOOD LUCK �
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
 
-  const rows = text.split('\n');
-  console.log(rows);
+//   const rows = text.split('\n');
+//   console.log(rows);
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
 
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
 
-    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
-  }
-});
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
 
-// we are going to use the current index in each iteration.
-// Now remember how we can get access
-// to the current index in the for of loop.
-// So we're looping over an array here,
-// and so to get the current index,
-// we need to actually use the entries
-// of the array, remember that?
+// // we are going to use the current index in each iteration.
+// // Now remember how we can get access
+// // to the current index in the for of loop.
+// // So we're looping over an array here,
+// // and so to get the current index,
+// // we need to actually use the entries
+// // of the array, remember that?
+
+/////////////////////////////////////////////////////////////////////
+// String Methods Practice  - One more String exercise
+/////////////////////////////////////////////////////////////////////
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+//   So let's pretend for example that we get this string here
+// from some kind of web API.
+// And our goal in our application
+// is tend to display the data like this.
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
